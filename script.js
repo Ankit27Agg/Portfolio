0b5090a64d22a7cbf7f111a9ba308dc85bf5b401
Ankit Aggarwal <65785638+Ankit27Agg@users.noreply.github.com>
2022-01-27 01:30:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('start')
-const light  = document.getElementById('light')
-const text = document.getElementById('text')
-
-function func(){
-  button.classList.toggle('active')
-  light.classList.toggle('on')
-  text.classList.toggle('text')
-}
