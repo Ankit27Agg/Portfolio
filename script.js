023ff02a32315d2551e2aee4fdee9b1b7fbe14f6
Ankit27Agg <worltresting@gmail.com>
2022-01-27 21:08:52 +0530

    changes

--- script.js
+++ script.js
@@ -12,3 +12,45 @@ function func(){
     logo.textContent = 'Ankit Aggarwal'
   }
 }
+
+const namee = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+let success = document.getElementsByClassName('success');
+const error = document.querySelectorAll('.error');
+console.dir(success)
+
+function validateForm(){
+  clearMessage();
+  let errorFlag=false;
+
+  if(namee.value.length<1){
+    error[0].innerText = 'Name can not be Empty';
+    errorFlag = true;
+  }
+
+  if(!emailIsValid(email.value)){
+    error[1].innerText = 'Email is not valid';
+    errorFlag = true;
+  }
+  if(message.value.length<1){
+    error[2].innerText = 'Message is Empty';
+    errorFlag = true;
+  }
+  if(!errorFlag){
+    success[0].innerText = "Success";
+    console.log(success.innerText)
+    console.log(errorFlag)
+  }
+}
+
+function clearMessage(){
+  for(let temp of error){
+    temp.innerText='';
+  }
+  success='';
+}
+function emailIsValid(email){
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
+}
